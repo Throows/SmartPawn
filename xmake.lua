38c089460e082544ac5765ed6bd0e8f68bd86757
Romain Berthoule <romain1.berthoule@gmail.com>
2023-03-04 11:13:01 +0100

    update xmake for win users

--- xmake.lua
+++ xmake.lua
@@ -1,10 +1,22 @@
 add_rules("mode.debug", "mode.release")
 add_rules("plugin.vsxmake.autoupdate")
-add_requires("spdlog", "brew::sfml", "brew::pybind11", "python")
-set_runtimes("MD")
+add_requires("spdlog", "sfml", "pybind11", "python")
 set_languages("cxx20")
 set_rundir("./bin/$(os)_$(arch)_$(mode)")
 set_targetdir("./bin/$(os)_$(arch)_$(mode)")
+set_symbols("debug", "hidden")
+
+if is_mode("release") then 
+    set_runtimes("MD")
+    if is_plat("windows") then
+        add_cxxflags("/SUBSYSTEM:WINDOWS", "/ENTRY:WinMainCRTStartup")
+    end 
+elseif is_mode("debug") then
+    set_runtimes("MDd")
+    if is_plat("windows") then
+        add_cxxflags("/SUBSYSTEM:CONSOLE", "/ENTRY:mainCRTStartup")
+    end
+end
 
 set_project("SmartPawn")
 
@@ -15,7 +27,7 @@ target("SPEngine")
     add_headerfiles("src/SPEngine/**.hpp")
     add_includedirs("src/SPEngine", {public = true})
     add_files("src/SPEngine/**.cpp")
-    add_packages("spdlog", "brew::sfml", "brew::pybind11", "python") 
+    add_packages("spdlog", "sfml", "pybind11", "python") 
 
     if is_plat("windows") then
         add_syslinks("opengl32")
@@ -23,10 +35,7 @@ target("SPEngine")
     elseif is_plat("macosx") then
         add_frameworks("CoreFoundation", "CoreGraphics", "CoreText", "CoreServices")
         add_defines("SP_PLATFORM_DARWIN")
-    end
-
-    set_symbols("debug", "hidden")
-    set_runtimes("MDd")
+    end 
     
 target_end()
 
@@ -39,7 +48,7 @@ target("SmartPawnViewer")
     add_includedirs("src/smartpawnviewer")
     add_files("src/smartpawnviewer/**.cpp")
     set_pcxxheader("src/smartpawnviewer/Core/pch.hpp");
-    add_packages("spdlog", "brew::sfml", "brew::pybind11", "python") 
+    add_packages("spdlog", "sfml", "pybind11", "python") 
 
     if is_plat("windows") then
         add_syslinks("opengl32")
@@ -48,8 +57,6 @@ target("SmartPawnViewer")
         add_frameworks("CoreFoundation", "CoreGraphics", "CoreText", "CoreServices")
         add_defines("SP_PLATFORM_DARWIN")
     end
-
-    set_symbols("debug", "hidden")
     
     after_build(function (target) 
         os.cp("$(projectdir)/resources/*", "$(projectdir)/bin/$(os)_$(arch)_$(mode)/resources/")
